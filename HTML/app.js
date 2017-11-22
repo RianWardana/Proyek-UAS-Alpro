60495c8e8ee62b8e7868790236f59f65a7b3443b
Rian Wardana <gmail@rianwardana.com>
2017-11-22 12:50:12 +0700

    Newest update

--- HTML/app.js
+++ HTML/app.js
@@ -1,21 +1,64 @@
 (function() {
-    var baseUrl = "http://103.43.44.105/alpro/index.php/app/read/";
+    var baseUrl = "http://103.43.44.105/alpro/index.php/app";
     var count = 1;
-    setInterval(function() {
-        var xhttp;
+    var name;
+
+    function sendName() {
+    	if (textName.value != "") {
+    		name = textName.value
+    		textName.value = "";
+    		formName.style.display = "none";
+    		formMsg.style.display = "block";
+    		textMsg.focus();
+    	}
+    }
+
+    function sendMsg() {
+    	if (textMsg.value != "") {
+    		var xhttp = new XMLHttpRequest();
+    		xhttp.open("POST", `${baseUrl}/send`, true);
+			xhttp.send(`${name}: ${textMsg.value}\r\n`);
+    		textMsg.value = "";
+    	}
+    }
+
+    textName.addEventListener('keypress', e => {
+    	var key = e.which || e.keyCode;
+    	if (key === 13) sendName();
+    });
+
+    textMsg.addEventListener('keypress', e => {
+    	var key = e.which || e.keyCode;
+    	if (key === 13) sendMsg();
+    });
+
+    btnSendName.addEventListener('click', e => {sendName()});
+    btnSendMsg.addEventListener('click', e => {sendMsg();});
+
+    function loop() {
+    	var xhttp;
         xhttp = new XMLHttpRequest();
 
-        xhttp.open("GET", `${baseUrl}${count}`, true);
+        xhttp.open("GET", `${baseUrl}/read/${count}`, true);
         xhttp.send();
 
-        setTimeout(function() {
-            if (xhttp.responseText != 'X') {
-                console.log(xhttp.responseText);
+        xhttp.onreadystatechange = function() {
+            if ((xhttp.responseText != 'X') && (xhttp.readyState == 4)) {
+                console.log(`Message ${count}-> ${xhttp.responseText}`);
                 var oldHTML = container.innerHTML;
                 var newHTML = '<p>' + xhttp.responseText + '</p>';
                 container.innerHTML = oldHTML + newHTML;
+    			container.scrollTop = container.scrollHeight;
                 count++;
+
+                setTimeout(function() {
+        			loop();
+        		}, 100);
+            } else if (xhttp.responseText == 'X') {
+            	setTimeout(function() {
+        			loop();
+        		}, 1000);
             }
-        }, 100);
-    }, 1000);
+        };
+    } loop();
 }())
